0fdbbc9b125671a381658c6fee3270fa47bd812e
binibin-k <binibinkk@gmail.com>
2022-10-26 19:04:50 +0900

    FBV에서 CBV로구현방식 수정

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:num>/", views.single_post_page),
+    # path("", views.index, name="index"),
+    path("", views.PostList.as_view(), name="post_list"),
+    # path("<int:num>/", views.single_post_page),
+    path("<int:num>/", views.PostDetail.as_view(), name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
-def index(request):
-    posts = Post.objects.all().order_by('-created_at')
-    return render(request, 'blog/index.html', {'posts': posts})
+# def index(request):
+#     posts = Post.objects.all().order_by('-created_at')
+#     return render(request, 'blog/index.html', {'posts': posts})
 
-def single_post_page(request, num):
-    post = Post.objects.get(pk=num)
+class PostList(ListView):
+    model = Post # Post 라는 클래스의 데이터를 가져옴
+    template_name = 'blog/index.html'
 
-    return render(request, 'blog/single_post_page.html', {'post':post})
+
+# def single_post_page(request, num):
+#     post = Post.objects.get(pk=num)
+#     return render(request, 'blog/single_post_page.html', {'post':post})
+
+class PostDetail(DetailView):
+    post = Post
+    template_name = 'blog/single_post_page.html'

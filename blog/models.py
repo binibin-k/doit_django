6d48bc7ee22b9c31692a45fe7c649992f8213885
binibin-k <binibinkk@gmail.com>
2022-10-26 17:02:48 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,7 @@ class Post(models.Model):
 
     def __str__(self):
         return f'[{self.pk}] {self.title}'
+    
+    def get_absolute_url(self): # 모델의 레코드별 URL 생성 규칙을 정의
+        return f'/blog/{self.pk}'
+        
--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,8 @@ from .models import Post
 def index(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'blog/index.html', {'posts': posts})
+
+def single_post_page(request, num):
+    post = Post.objects.get(pk=num)
+
+    return render(request, 'blog/single_post_page.html', {'post':post})

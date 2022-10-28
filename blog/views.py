f0ae405649e754479e205e7e5bd7bdc22a5129bd
binibin-k <binibinkk@gmail.com>
2022-10-28 12:50:44 +0900

    카테고리별 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -17,9 +17,24 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
-# def single_post_page(request, num):
-#     post = Post.objects.get(pk=num)
-#     return render(request, 'blog/single_post_page.html', {'post':post})
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category
+        }
+    )
 
 class PostDetail(DetailView):
     model = Post

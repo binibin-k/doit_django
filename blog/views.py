384babd4835ae27a654d66405fd457b5c3e20eca
binibin-k <binibinkk@gmail.com>
2022-10-28 11:31:45 +0900

    카테고리 필드 추가 및 포스트 목록 페이지에 카테고리 기능 적용

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from .models import Post
+from .models import Post, Category
 
 # Create your views here.
 # def index(request):
@@ -10,8 +10,12 @@ from .models import Post
 class PostList(ListView):
     model = Post # Post 라는 클래스의 데이터를 가져옴
     ordering = ['-pk']
-    # template_name = 'blog/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(PostList, self).get_context_data()
+        context['categories'] = Category.objects.all()
+        context['no_category_post_count'] = Post.objects.filter(category=None).count()
+        return context
 
 # def single_post_page(request, num):
 #     post = Post.objects.get(pk=num)
@@ -19,4 +23,3 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
-    # template_name = 'blog/single_post_page.html'
